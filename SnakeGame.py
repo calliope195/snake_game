833308447f0ac223d61d398b503ff9e2380499c6
Rylle <verdaderkarylle@gmail.com>
2024-07-24 06:51:56 +0800

    variable error fixed

--- SnakeGame.py
+++ SnakeGame.py
@@ -6,14 +6,14 @@ delay=0.1
 score = 0
 high_score = 0
 
-#the window height and width
+# Creating the window and setting height and width
 wn = turtle.Screen()
-wn.title("Rylle's Snake Game")
+wn.title("Snake Game")
 wn.bgcolor("black")
 wn.setup(width=700, height=700)
 wn.tracer(0)
 
-#Game Border
+#Creating a border for the game
 turtle.speed(5)
 turtle.pensize(4)
 turtle.penup()
@@ -30,52 +30,58 @@ turtle.forward(500)
 turtle.penup()
 turtle.hideturtle()
 
-#snake head
+# Crerating head of the snake
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
 head.color("white")
 head.penup()
-head.goto(0,0)
+head.goto(0, 0)
 head.direction = "Stop"
 
-#Food
+
+#Creating food in the game
 food = turtle.Turtle()
 food_color = random.choice(['yellow', 'green', 'tomato'])
-food_shape = random.choice(['triangle', 'circle', 'square'])
+food_shape = random.choice([ 'triangle', 'circle','square'])
 food.speed(0)
 food.shape(food_shape)
 food.color(food_color)
 food.penup()
-food.goto(20,20)
+food.goto(20, 20)
 
-#space for score
+#Creating space to show score and high score
 scoreBoard = turtle.Turtle()
 scoreBoard.speed(0)
 scoreBoard.shape("square")
-score.color("white")
+scoreBoard.color("white")
 scoreBoard.penup()
 scoreBoard.hideturtle()
 scoreBoard.goto(0, 250)
 scoreBoard.write("Score : 0 High Score : 0", align="center",
-    font=("Courier", 25,"bold"))
+        font=("Courier", 25, "bold"))
 
-#Key Directions
+
+# assigning key directions
 def move_up():
     if head.direction != "down":
         head.direction = "up"
+
 
 def move_down():
     if head.direction != "up":
         head.direction = "down"
 
+
 def move_left():
     if head.direction != "right":
-        head.direction = "right"
+        head.direction = "left"
+
 
 def move_right():
-    if head.direction !="left":
+    if head.direction != "left":
         head.direction = "right"
+
 
 def move():
     if head.direction == "up":
@@ -91,7 +97,8 @@ def move():
         x = head.xcor()
         head.setx(x+20)
 
-wn.listen
+
+wn.listen()
 wn.onkeypress(move_up, "Up")
 wn.onkeypress(move_down, "Down")
 wn.onkeypress(move_left, "Left")
@@ -99,38 +106,39 @@ wn.onkeypress(move_right, "Right")
 
 segments = []
 
-#Main Game
-while True:
+# Main Game
+while True: #Running an infinite till the collision occurs and then game ends
     wn.update()
 
-    if head.xcor() > 250 or head.xcor() < -300 or head.ycor() > 240 or head.ycor() <-240:
+    #Ending the game on collision with any of the walls
+    if head.xcor() > 280 or head.xcor() < -300 or head.ycor() > 240 or head.ycor() < -240:
         time.sleep(1)
-        wn.clear()
         wn.bgcolor('blue')
         scoreBoard.goto(0,0)
         scoreBoard.write("GAME OVER\n Your Score is : {}".format(
             score), align="center", font=("Courier", 30, "bold"))
 
-#if snake collect food
+    #If snake collects food
     if head.distance(food) < 20:
+        #increasing score and updating the high_score if required
         score += 10
         if score > high_score:
             high_score = score
         scoreBoard.clear()
-        score.Board.write("Score : {} High Score : {} ".format(
-         score, high_score), align="center", font=("Courier", 25, "bold"))
+        scoreBoard.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("Courier", 25, "bold"))
 
-#creating food at random location
+        #creating food at random location
         x_cord = random.randint(-290, 270)
         y_cord = random.randint(-240, 240)
         food_color = random.choice(['yellow', 'green', 'tomato'])
-        food_shape = random.choice([ 'triangle', 'circle', 'square'])
+        food_shape = random.choice([ 'triangle', 'circle','square'])
         food.speed(0)
         food.shape(food_shape)
         food.color(food_color)
         food.goto(x_cord, y_cord)
 
-
+        # Adding a new segment to the snake
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
@@ -138,27 +146,28 @@ while True:
         new_segment.penup()
         segments.append(new_segment) #adding the segment to the list
 
-#Moving the snake
+        
+    # Moving the snake and ending the game on collisions of head with body segments
     for i in range(len(segments)-1, 0, -1):
-        x = segment[i-1].xcor()
-        y = segment[i-1].ycor()
-        segments[i].goto(x,y)
+        x = segments[i-1].xcor()
+        y = segments[i-1].ycor()
+        segments[i].goto(x, y)
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
-        segments(0).goto(x,y)
+        segments[0].goto(x, y)
     move()
 
-#Checking for collision wiht the body
+    #Checking for collision with the body
     for segment in segments:
         if segment.distance(head) < 20:
             time.sleep(1)
             wn.clear()
             wn.bgcolor('blue')
             scoreBoard.goto(0,0)
-            scoreBoard.write("\t\tGAME OVER\n Your Score is : ()".format(
+            scoreBoard.write("\t\tGAME OVER\n Your Score is : {}".format(
                 score), align="center", font=("Courier", 30, "bold"))
-        
+
     time.sleep(delay)
 
 turtle.Terminator()
